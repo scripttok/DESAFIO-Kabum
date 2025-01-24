5297627df61c72cf7bbce7158c61802cb67dd4b7
LogicMaster <traderrafa02@gmail.com>
2025-01-24 16:34:43 -0300

    adds registration screen and customer screen

--- app.js
+++ app.js
@@ -1,25 +1,76 @@
 const express = require("express");
-const app = express();
+const mysql = require("mysql2");
+const bodyParser = require("body-parser");
 const path = require("path");
 
-// const cadastrado = document.querySelector("#cadastrar");
-// console.log(cadastrado);
+const app = express();
 
-// Rota para servir a página inicial
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "home.html")); // Caminho absoluto para o arquivo home.html
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "xablau",
+  password: "12345",
+  database: "mybanc",
 });
 
-// Rota para servir a página de usuário
-app.get("/user", (req, res) => {
-  res.sendFile(path.join(__dirname, "user.html"));
+db.connect((err) => {
+  if (err) {
+    console.error("Erro ao conectar ao banco de dados:", err);
+  } else {
+    console.log("Conectado ao banco de dados");
+  }
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "home.html"));
 });
 
 app.get("/cadastro", (req, res) => {
   res.sendFile(path.join(__dirname, "cadastro.html"));
 });
 
-// Inicializando o servidor
+app.get("/user", (req, res) => {
+  res.sendFile(path.join(__dirname, "user.html"));
+});
+
+app.post("/cadastro", (req, res) => {
+  const { nome, data_nascimento, cpf, rg, telefone } = req.body;
+
+  const query = `
+    INSERT INTO clientes (nome, data_nascimento, cpf, rg, telefone)
+    VALUES (?, ?, ?, ?, ?)
+  `;
+
+  db.query(
+    query,
+    [nome, data_nascimento, cpf, rg, telefone],
+    (err, results) => {
+      if (err) {
+        console.error("Erro ao salvar os dados:", err);
+        return res.status(500).send("Erro ao salvar os dados");
+      }
+      console.log("Cliente cadastrado com sucesso:", results);
+      res.redirect("/");
+    }
+  );
+});
+
+app.get("/clientes", (req, res) => {
+  const query = "SELECT * FROM clientes";
+
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error("Erro ao buscar os clientes:", err);
+      return res.status(500).send("Erro ao buscar os clientes");
+    }
+
+    res.render("clientes", { clientes: results });
+  });
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
