b79396253f2cd4b19cb8bcaa1eda151dac5a27bd
LogicMaster <traderrafa02@gmail.com>
2025-01-26 08:39:51 -0300

    change the registration screen change the customer list

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mysql = require("mysql2");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 const path = require("path");
+const e = require("method-override");
 
 const app = express();
 
@@ -34,30 +35,12 @@ app.get("/cadastro", (req, res) => {
   res.sendFile(path.join(__dirname, "cadastro.html"));
 });
 
-app.post("/cadastro", (req, res) => {
-  const { nome, data_nascimento, cpf, rg, telefone } = req.body;
-
-  const query = `
-    INSERT INTO clientes (nome, data_nascimento, cpf, rg, telefone)
-    VALUES (?, ?, ?, ?, ?)
-  `;
-
-  db.query(
-    query,
-    [nome, data_nascimento, cpf, rg, telefone],
-    (err, results) => {
-      if (err) {
-        console.error("Erro ao salvar os dados:", err);
-        return res.status(500).send("Erro ao salvar os dados");
-      }
-      console.log("Cliente cadastrado com sucesso:", results);
-      res.redirect("/clientes");
-    }
-  );
-});
-
 app.get("/clientes", (req, res) => {
-  const query = "SELECT * FROM clientes";
+  const query = `
+    SELECT clientes.*, enderecos.endereco, enderecos.cidade, enderecos.estado, enderecos.cep
+    FROM clientes
+    LEFT JOIN enderecos ON clientes.id = enderecos.cliente_id
+  `;
 
   db.query(query, (err, results) => {
     if (err) {
@@ -80,44 +63,154 @@ app.get("/cliente/:id/editar", (req, res) => {
       return res.status(500).send("Erro ao buscar cliente");
     }
 
-    res.render("editar", { cliente: results[0] });
+    const queryEnderecos = "SELECT * FROM enderecos WHERE cliente_id = ?";
+    db.query(queryEnderecos, [id], (err, enderecos) => {
+      if (err) {
+        console.error("Erro ao buscar endereços:", err);
+        return res.status(500).send("Erro ao buscar endereços");
+      }
+
+      res.render("editar", { cliente: results[0], enderecos });
+    });
   });
+});
+
+app.post("/cadastro", (req, res) => {
+  const {
+    nome,
+    data_nascimento,
+    cpf,
+    rg,
+    telefone,
+    endereco,
+    cidade,
+    estado,
+    cep,
+  } = req.body;
+
+  const queryCliente = `
+    INSERT INTO clientes (nome, data_nascimento, cpf, rg, telefone)
+    VALUES (?, ?, ?, ?, ?)
+  `;
+
+  db.query(
+    queryCliente,
+    [nome, data_nascimento, cpf, rg, telefone],
+    (err, result) => {
+      if (err) {
+        console.error("Erro ao cadastrar cliente:", err);
+        return res.status(500).send("Erro ao cadastrar cliente");
+      }
+
+      const clienteId = result.insertId;
+
+      if (endereco && cidade && estado && cep) {
+        const queryEndereco = `
+          INSERT INTO enderecos (cliente_id, endereco, cidade, estado, cep)
+          VALUES (?, ?, ?, ?, ?)
+        `;
+
+        db.query(
+          queryEndereco,
+          [clienteId, endereco, cidade, estado, cep],
+          (err) => {
+            if (err) {
+              console.error("Erro ao cadastrar endereço:", err);
+              return res.status(500).send("Erro ao cadastrar endereço");
+            }
+
+            res.redirect("/clientes");
+          }
+        );
+      } else {
+        res.redirect("/clientes");
+      }
+    }
+  );
 });
 
 app.put("/cliente/:id", (req, res) => {
   const { id } = req.params;
-  const { nome, data_nascimento, cpf, rg, telefone } = req.body;
+  const { nome, data_nascimento, cpf, rg, telefone, enderecos } = req.body;
 
-  const query = `
+  const queryCliente = `
     UPDATE clientes
     SET nome = ?, data_nascimento = ?, cpf = ?, rg = ?, telefone = ?
     WHERE id = ?
   `;
 
-  db.query(query, [nome, data_nascimento, cpf, rg, telefone, id], (err) => {
-    if (err) {
-      console.error("Erro ao atualizar cliente:", err);
-      return res.status(500).send("Erro ao atualizar cliente");
+  db.query(
+    queryCliente,
+    [nome, data_nascimento, cpf, rg, telefone, id],
+    (err) => {
+      if (err) {
+        console.error("Erro ao atualizar cliente:", err);
+        return res.status(500).send("Erro ao atualizar cliente");
+      }
+
+      const deleteQuery = `
+        DELETE FROM enderecos WHERE cliente_id = ?
+      `;
+
+      db.query(deleteQuery, [id], (err) => {
+        if (err) {
+          console.error("Erro ao apagar os endereços antigos:", err);
+          return res.status(500).send("Erro ao atualizar os endereços");
+        }
+
+        if (enderecos && enderecos.length > 0) {
+          const insertQuery = `
+            INSERT INTO enderecos (cliente_id, endereco, cidade, estado, cep)
+            VALUES (?, ?, ?, ?, ?)
+          `;
+
+          enderecos.forEach((endereco) => {
+            db.query(
+              insertQuery,
+              [
+                id,
+                endereco.endereco,
+                endereco.cidade || null,
+                endereco.estado || null,
+                endereco.cep || null,
+              ],
+              (err) => {
+                if (err) {
+                  console.error("Erro ao inserir novo endereço:", err);
+                  return res.status(500).send("Erro ao inserir novo endereço");
+                }
+              }
+            );
+          });
+        }
+
+        res.redirect("/clientes");
+      });
     }
-
-    res.redirect("/clientes");
-  });
+  );
 });
-
-// metodo delete
 
 app.delete("/cliente/:id", (req, res) => {
   const clienteId = req.params.id;
 
-  const query = "DELETE FROM clientes WHERE id = ?";
-  db.query(query, [clienteId], (err, results) => {
+  const deleteEnderecosQuery = "DELETE FROM enderecos WHERE cliente_id = ?";
+
+  db.query(deleteEnderecosQuery, [clienteId], (err) => {
     if (err) {
-      console.error("Erro ao deletar o cliente:", err);
-      return res.status(500).send("Erro ao deletar o cliente");
+      console.error("Erro ao apagar endereços:", err);
+      return res.status(500).send("Erro ao apagar endereços");
     }
 
-    console.log(`Cliente com ID ${clienteId} deletado com sucesso`);
-    res.redirect("/clientes");
+    const deleteClienteQuery = "DELETE FROM clientes WHERE id = ?";
+    db.query(deleteClienteQuery, [clienteId], (err, results) => {
+      if (err) {
+        console.error("Erro ao deletar o cliente:", err);
+        return res.status(500).send("Erro ao deletar o cliente");
+      }
+
+      console.log(`Cliente com ID ${clienteId} deletado com sucesso`);
+      res.redirect("/clientes");
+    });
   });
 });
 
